80e683d35247205f9e6d3d5b56cd8b1b95a5139a
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-20 15:52:30 +0600

    next time

---
+++ math.js
@@ -0,0 +1,11 @@
+
+const max= Math.max(23, 3445, 55, 66, 7);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(59 - 90));
+console.log(Math.round(4.6))
+console.log(Math.floor(4.5));
+console.log(Math.ceil(5.3));
+console.log(Math.random()*10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
